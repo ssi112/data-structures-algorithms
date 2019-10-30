2c6d7764df3499e8568808dbfc5fdc065938346f
ssi112 <brrg58@protonmail.com>
2019-10-29 20:28:54 -0400

    prob-4-6

---
+++ project3/4_problem.py
@@ -0,0 +1,58 @@
+"""
+4_problem.py
+
+Given an input array consisting on only 0, 1, and 2,
+sort the array in a single traversal. You're not allowed
+to use any sorting function that Python provides.
+
+Note: O(n) does not necessarily mean single-traversal.
+For example if you traverse the array twice, that would still
+be an O(n) solution but it will not count as single traversal.
+"""
+
+def sort_012(input_list):
+    """
+    The idea is to put 0 and 2 in their correct positions,
+    which will make sure all the 1s are automatically placed
+    in their right positions
+    """
+
+    # initialize pointers for next positions of 0 and 2
+    next_pos_0 = 0
+    next_pos_2 = len(input_list) - 1
+
+    front_index = 0
+
+    while front_index <= next_pos_2:
+        if input_list[front_index] == 0:
+            input_list[front_index] = input_list[next_pos_0]
+            input_list[next_pos_0] = 0
+            next_pos_0 += 1
+            front_index += 1
+        elif input_list[front_index] == 2:
+            input_list[front_index] = input_list[next_pos_2]
+            input_list[next_pos_2] = 2
+            next_pos_2 -= 1
+        else:
+            front_index += 1
+    return input_list
+
+
+def test_function(test_case):
+    sorted_array = sort_012(test_case)
+    print(sorted_array)
+    if sorted_array == sorted(test_case):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+test_case = [0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2]
+test_function(test_case)
+
+test_case = [2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1]
+test_function(test_case)
+
+test_case = [2, 2, 0, 0, 2, 1, 0, 2, 2, 1, 1, 1, 0, 1, 2, 0, 2, 0, 1]
+test_function(test_case)
+
---
+++ project3/6_problem.py
@@ -0,0 +1,53 @@
+"""
+6_problem.py
+
+In this problem, we will look for smallest and largest integer from a list of
+unsorted integers. The code should run in O(n) time. Do not use Python's
+built-in functions to find min and max.
+
+Bonus Challenge: Is it possible to find the max and min in a single traversal?
+"""
+
+def find_min_max(input_list):
+    min = 0
+    max = 0
+
+    if len(input_list) == 0:
+        return (False, False)
+    if len(input_list) == 1:
+        min = input_list[0]
+        max = input_list[0]
+        return (min, max)
+
+    # initialize min and max with first two elements
+    if input_list[0] < input_list[1]:
+        min = input_list[0]
+        max = input_list[1]
+    else:
+        min = input_list[1]
+        max = input_list[0]
+    # walk the rest of the list and update min/max as needed
+    # could use enumerate but value is not needed
+    # plus the index begins at zero anyway
+    # and we need to skip the first two elements
+    for indx in range(len(input_list[2:])+2):
+        if input_list[indx] > max:
+            max = input_list[indx]
+        elif input_list[indx] < min:
+            min = input_list[indx]
+        indx += 1
+    return (min, max)
+
+
+## Example Test Case of Ten Integers
+import random
+
+l = [i for i in range(0, 10)]  # a list containing 0 - 9
+random.shuffle(l)
+
+print("Pass" if ((0, 9) == find_min_max(l)) else "Fail")
+
+print("min max", find_min_max([]))
+print("min max", find_min_max([99]))
+print("min max", find_min_max([0,0,-1,-1,0,-2,1,0,2,1]))
+print("min max", find_min_max([99,99]))
