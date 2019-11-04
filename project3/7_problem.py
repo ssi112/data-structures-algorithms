2e8d19b678663da8ecfe977d3480610bfc6f9e45
ssi112 <brrg58@protonmail.com>
2019-11-04 17:48:44 -0500

    began prob 7 new branch soon

--- project3/7_problem.py
+++ project3/7_problem.py
@@ -12,16 +12,37 @@ block of code called a handler.
 
 """
 
+
+"""
+A RouteTrieNode will be similar to our autocomplete TrieNode...
+with one additional element, a handler.
+"""
+class RouteTrieNode:
+    def __init__(self, handler = None):
+        # Initialize the node with children as before, plus a handler
+        self.children = dict()
+        self.handler = handler
+
+    def insert(self, path, handler):
+        # Insert the node as before
+        for path_part in path:
+            self.children[path_part]
+        if handler:
+            self.handler = handler
+        else:
+            self.handler = None
+
+
 # A RouteTrie will store our routes and their associated handlers
 class RouteTrie:
-    def __init__(self, ...):
+    def __init__(self):
         """
         Initialize the trie with a root node and a handler,
         this is the root path or home page node
         """
 
 
-    def insert(self, ...):
+    def insert(self):
         """
         Similar to our previous example you will want to recursively
         add nodes. Make sure you assign the handler to only the leaf
@@ -29,39 +50,28 @@ class RouteTrie:
         """
 
 
-    def find(self, ...):
+    def find(self):
         """
         Starting at the root, navigate the Trie to find a match for
         this path. Return the handler for a match, or None for no match
         """
 
-"""
-A RouteTrieNode will be similar to our autocomplete TrieNode...
-with one additional element, a handler.
-"""
-class RouteTrieNode:
-    def __init__(self, ...):
-        # Initialize the node with children as before, plus a handler
-
-    def insert(self, ...):
-        # Insert the node as before
-
 
 # The Router class will wrap the Trie and handle
 class Router:
-    def __init__(self, ...):
+    def __init__(self):
         """
         Create a new RouteTrie for holding our routes. You could also
         add a handler for 404 page not found responses as well!
         """
 
-    def add_handler(self, ...):
+    def add_handler(self):
         """
         Add a handler for a path. You will need to split the path and
         pass the pass parts as a list to the RouteTrie
         """
 
-    def lookup(self, ...):
+    def lookup(self):
         """
         Lookup path (by parts) and return the associated handler.
         You can return None if it's not found or return the "not found"
@@ -71,10 +81,10 @@ class Router:
         """
 
 
-    def split_path(self, ...):
+    def split_path(self):
         """
         You need to split the path into parts for both the add_handler
-        and loopup functions, so it should be placed in a function here
+        and lookup functions, so it should be placed in a function here
         """
 
 
